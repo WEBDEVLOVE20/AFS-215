be2ddfba319cc140abf1cd616e7a3aaa58b6d9cf
Kelly <kelly.compton@bryanuniversity.edu>
2021-03-22 22:06:11 -0400

    #

---
+++ week1/project1/function.py
@@ -0,0 +1,12 @@
+import unittest
+
+class Test_PerfectNumber(unittest.TestCase):
+
+    def perfect_number(self):
+        sum = 0
+        for x in range(1, self):
+            if self % x == 0:
+                sum += x
+        return sum == self
+
+    print(perfect_number(8))
